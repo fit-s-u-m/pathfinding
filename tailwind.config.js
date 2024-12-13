412dfdcaa4b47c036fc3a49d589b7c2bbd325ea9
fit-s-u-m <nehemiahmaths@gmail.com>
2024-12-13 18:14:54 +0300

    added some consistant colors in html and css

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,33 @@ export default {
   theme: {
     extend: {
       colors: {
-        "primary-color": "#554971",
-        "primary-color-light": "#BCD8B7",
-        "secondary-color": "#A2999E",
-        "accent-color": "#ff6380",
-        "background-color": "#C6C7C4",
-        "bold-color": "#000",
+        'primary-color': {
+          '50': '#f4f3fa',
+          '100': '#e8e8f7',
+          '200': '#d8d6ef',
+          '300': '#c0bde4',
+          '400': '#aba2d7',
+          '500': '#9a8aca',
+          '600': '#8971ba',
+          '700': '#7660a2',
+          '800': '#614f84',
+          '900': '#554971',
+          '950': '#2f283e',
+        },
+        'background-color': {
+          '50': '#f8f8f8',
+          '100': '#f2f1f1',
+          '200': '#e7e4e6',
+          '300': '#d3ced1',
+          '400': '#bab2b6',
+          '500': '#a2999e',
+          '600': '#847a80',
+          '700': '#6c6469',
+          '800': '#5c555a',
+          '900': '#514a4f',
+          '950': '#2a2729',
+        },
+
       },
 
     },
@@ -23,3 +44,9 @@ export default {
   ],
 }
 
+        // "primary-color": "#554971",
+        // "primary-color-light": "#BCD8B7",
+        // "secondary-color": "#A2999E",
+        // "accent-color": "#ff6380",
+        // "background-color": "#C6C7C4",
+        // "bold-color": "#000",
